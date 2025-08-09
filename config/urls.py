63c36c6a2fada4a659f971599450a42c258e6015
anava98 <anavarro776@gmail.com>
2025-08-09 15:05:02 -0400

    Class 4

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ from django.urls import path ,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('pages.urls')),
+    path('posts/',include ('posts.urls')),
     
     
 ]
